fa8aea5ac6e47ad1b5adc4445f74a58a00e6d30a
Maksim Matlakhov <makcon2003@gmail.com>
2021-06-23 13:27:16 +0200

    fix sql script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO users VALUES (1, 'David', 100, 4);
-INSERT INTO users VALUES (2, 'Arnold', 200, 2);
-INSERT INTO users VALUES (3, 'Ilona', 150, 3);
+INSERT INTO users VALUES (1, 'David', 100);
+INSERT INTO users VALUES (2, 'Arnold', 200);
+INSERT INTO users VALUES (3, 'Ilona', 150);
 
 INSERT INTO payment_methods (user_id, name) VALUES (1, 'My bank account');
 INSERT INTO payment_methods (user_id, name) VALUES (1, 'My mom account');
