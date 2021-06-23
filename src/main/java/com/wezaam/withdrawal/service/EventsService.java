c28341e11bb0d2917730a390fb09329ade71e79d
Maksim Matlakhov <makcon2003@gmail.com>
2021-06-23 13:22:13 +0200

    send events async

--- src/main/java/com/wezaam/withdrawal/service/EventsService.java
+++ src/main/java/com/wezaam/withdrawal/service/EventsService.java
@@ -2,16 +2,19 @@ package com.wezaam.withdrawal.service;
 
 import com.wezaam.withdrawal.model.Withdrawal;
 import com.wezaam.withdrawal.model.WithdrawalScheduled;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventsService {
 
+    @Async
     public void send(Withdrawal withdrawal) {
-        // send an event in message queue
+        // build and send an event in message queue async
     }
 
+    @Async
     public void send(WithdrawalScheduled withdrawal) {
-        // send an event in message queue
+        // build and send an event in message queue async
     }
 }
