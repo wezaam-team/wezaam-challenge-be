97bd381cdf3f83f5a041cfb2e4adf6d5aefb7325
Pete W <pete.woznowski@smarsh.com>
2023-08-27 17:14:39 +0200

    Wezaam-cb-1 implementing first test

--- src/main/java/com/wezaam/withdrawal/model/User.java
+++ src/main/java/com/wezaam/withdrawal/model/User.java
@@ -1,5 +1,10 @@
 package com.wezaam.withdrawal.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,6 +13,10 @@ import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "users")
 public class User {
 
@@ -19,35 +28,4 @@ public class User {
     private List<PaymentMethod> paymentMethods;
     private Double maxWithdrawalAmount;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public List<PaymentMethod> getPaymentMethods() {
-        return paymentMethods;
-    }
-
-    public void setPaymentMethods(List<PaymentMethod> paymentMethods) {
-        this.paymentMethods = paymentMethods;
-    }
-
-    public Double getMaxWithdrawalAmount() {
-        return maxWithdrawalAmount;
-    }
-
-    public void setMaxWithdrawalAmount(Double maxWithdrawalAmount) {
-        this.maxWithdrawalAmount = maxWithdrawalAmount;
-    }
 }
--- src/main/java/com/wezaam/withdrawal/service/UserService.java
+++ src/main/java/com/wezaam/withdrawal/service/UserService.java
@@ -1,7 +1,23 @@
 package com.wezaam.withdrawal.service;
 
+import com.wezaam.withdrawal.model.User;
+import com.wezaam.withdrawal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User findById(Long id) {
+		return null;
+	}
+
+	public List<User> findAll(){
+		return null;
+	}
 }
